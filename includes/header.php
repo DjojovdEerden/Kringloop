1f93cbc6e9b6824a739d994067b7a50dacdf8b11
DjojovdEerden <djojovandereerden@gmail.com>
2026-01-28 12:56:35 +0100

    front end
    index header footer

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle ?? 'Kringloop centrum'; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand d-flex align-items-center" href="index.php">
+                <img src="assets/img/Logo Kringloop.png" alt="Logo" class="navbar-logo me-2">
+                Kringloop centrum
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="rittenDropdown" role="button" data-bs-toggle="dropdown">Ritten</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="">Ritten planning</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="voorraad.php">Voorraad</a></li>
+                    <li class="nav-item"><a class="nav-link" href="beheer.php">Beheer</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Admin</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="">Dashboard</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-light" href="login.php">Aanmelden</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
